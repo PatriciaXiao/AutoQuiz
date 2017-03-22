5afb59d82dc789702677e0c9fea149cd4aef66bb
Patricia Xiao <patricia.xiao@berkeley.edu>
2017-03-21 21:21:21 -0700

    bug fixed
    now the basic jumping logic between question and answer pages is correct

--- auto_quiz.py
+++ auto_quiz.py
@@ -9,6 +9,10 @@ def question():
 	answer_expected, cand_list = generate_cand_list(dec2bin, question_parameter, \
 		interval=4, amount=5, limit=True, lim_min = 0, lim_max=2*question_parameter)
 	session['cand_list'] = cand_list
+	session['answer_expected'] = answer_expected
+	session['question_parameter'] = question_parameter
+	session['question_id'] = question_id
+	'''
 	if request.method == 'POST':
 		# print "answer got {0}".format(request.form['candidate'])
 		if request.form['candidate'] == answer_expected:
@@ -19,6 +23,12 @@ def question():
 			question_id=question_id, \
 			question_parameter = question_parameter, \
 			selected=request.form['candidate']))
+		# called by question_id = request.args['question_id'] #session['messages']
+		# 	cand_list = session['cand_list']
+		# 	selected = request.args['selected']
+		# 	question_parameter = request.args['question_parameter']
+		# in function report_result
+	'''
 		
 	return render_template('single_question_layout.html', \
 		question_id = question_id, \
@@ -27,11 +37,11 @@ def question():
 
 @app.route('/result', methods=['GET', 'POST'])
 def report_result():
-	question_id = request.args['question_id'] #session['messages']
+	question_id = session['question_id']
 	cand_list = session['cand_list']
-	selected = request.args['selected']
-	question_parameter = request.args['question_parameter']
-	print "candidate list {0}, length {1}".format(session['cand_list'], len(session['cand_list']))
+	answer_expected = session['answer_expected']
+	question_parameter = session['question_parameter']
+	selected = request.form['candidate']
 	return render_template('single_result_layout.html', \
 		question_id = question_id, \
 		question_parameter = question_parameter, \
